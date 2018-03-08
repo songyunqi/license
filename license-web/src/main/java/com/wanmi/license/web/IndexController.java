7072336500dd0ea742a4666d0fb0b5902e37bb24
宋运旗 <songyunqi@qianmi.com>
2018-03-08 18:47:36 +0800

    Xx

--- license-web/src/main/java/com/wanmi/license/web/IndexController.java
+++ license-web/src/main/java/com/wanmi/license/web/IndexController.java
@@ -1,4 +1,17 @@
 package com.wanmi.license.web;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class IndexController {
+    @RequestMapping("index")
+    public String index(HttpServletRequest request) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        request.getSession().setAttribute("profile", principal);
+        return "index";
+    }
 }
