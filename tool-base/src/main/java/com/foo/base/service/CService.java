8cc578ca18ba6d0bcb6c18faac1318bef383acfd
宋运旗 <songyunqi@qianmi.com>
2018-03-15 13:43:49 +0800

    Xx

--- tool-base/src/main/java/com/foo/base/service/CService.java
+++ tool-base/src/main/java/com/foo/base/service/CService.java
@@ -1,8 +1,6 @@
 package com.foo.base.service;
 
 import com.foo.base.request.ARequest;
-import com.github.pagehelper.ISelect;
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
@@ -14,21 +12,6 @@ public abstract class CService<T, ID, TRequest extends ARequest> implements DDSe
 
     public PageInfo<T> page(final TRequest request) {
         PageInfo<T> pageInfo = PageHelper.startPage(request.getPageNum(), request.getPageSize()).doSelectPageInfo(() -> list(request));
-        return pageInfo;
-    }
-
-    public PageInfo<T> page(TRequest request, ISelect iSelect) {
-        //Long totalCount = PageHelper.count(iSelect);
-        //iSelect.
-        Page<?> page = PageHelper.startPage(request.getPageNum(), request.getPageSize(), false);
-        List<T> list = list(request);
-        PageInfo<T> pageInfo = new PageInfo(page, page.getPageSize());
-        pageInfo.setList(list);
-        //pageInfo.setTotal(totalCount);
-
-
-        pageInfo = PageHelper.startPage(request.getPageNum(), request.getPageSize()).doSelectPageInfo(iSelect);
-
         return pageInfo;
     }
 
